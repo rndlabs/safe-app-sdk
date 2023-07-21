a554a326a4b7f5f0ccd963b7c8e0e703bdced2cb
mfw78 <mfw78@protonmail.com>
2023-07-21 11:51:43 +0000

    chore: strip

--- packages/auth-kit/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './packs/web3auth/Web3AuthModalPack'
-export * from './packs/web3auth/types'
-
-export * from './AuthKitBasePack'
-export * from './types'
--- packages/auth-kit/src/types.ts
+++
@@ -1,4 +0,0 @@
-export type AuthKitSignInData = {
-  eoa: string
-  safes?: string[]
-}
